50c3f25168543c02233d784001a3f68e1a622802
Toby McLaughlin <toby@jarpy.net>
2016-11-07 23:10:04 -0500

    Update Java to latest Ubuntu security release.

--- test/constants.py
+++ test/constants.py
@@ -1,4 +1,4 @@
 import os
 
-java_version_string = 'openjdk version "1.8.0_91"'
+java_version_string = 'openjdk version "1.8.0_111"'
 logstash_version_string = 'logstash ' + os.environ['ELASTIC_VERSION']
