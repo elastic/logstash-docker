72198c487612929fd8748bd1760cc1bb1c3f2ff9
Toby McLaughlin <toby@jarpy.net>
2017-03-17 12:13:51 +1100

    Remove brittle Java version test

--- test/test_basics.py
+++ test/test_basics.py
@@ -2,10 +2,6 @@ from .helpers import environment, stdout_of, stderr_of
 from .constants import java_version_string, logstash_version_string
 
 
-def test_java_is_the_correct_version():
-    assert java_version_string in stderr_of('java -version')
-
-
 def test_logstash_is_the_correct_version():
     assert logstash_version_string in stdout_of('logstash --version')
 
