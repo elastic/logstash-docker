94e8ec20fe88d6e3eaab06e4668b62d6f7564699
Toby McLaughlin <toby@jarpy.net>
2017-03-28 01:46:50 +0200

    Test that env2yaml is optional and off by default

--- test/test_settings.py
+++ test/test_settings.py
@@ -2,15 +2,20 @@ from .fixtures import logstash
 
 
 def test_setting_pipeline_workers_from_environment(logstash):
-    logstash.restart(args='-e pipeline.workers=6')
+    logstash.restart(args='-e ENV2YAML=1 -e pipeline.workers=6')
     assert logstash.get_node_info()['pipeline']['workers'] == 6
 
 
 def test_setting_pipeline_batch_size_from_environment(logstash):
-    logstash.restart(args='-e pipeline.batch.size=123')
+    logstash.restart(args='-e env2yaml=1 -e pipeline.batch.size=123')
     assert logstash.get_node_info()['pipeline']['batch_size'] == 123
 
 
 def test_setting_pipeline_batch_delay_from_environment(logstash):
-    logstash.restart(args='-e pipeline.batch.delay=36')
+    logstash.restart(args='-e env2yaml=1 -e pipeline.batch.delay=36')
     assert logstash.get_node_info()['pipeline']['batch_delay'] == 36
+
+
+def test_not_opting_in_to_experimental_env2yaml_support(logstash):
+    logstash.restart(args='-e pipeline.batch.delay=47')  # No '-e env2yaml'
+    assert logstash.get_node_info()['pipeline']['batch_delay'] != 47
