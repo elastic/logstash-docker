c999e1ca9102bbce4789c1d3d487a5c6f3bd7146
Toby McLaughlin <toby@jarpy.net>
2017-03-28 01:36:49 +0200

    Fix support for staging builds

--- test/constants.py
+++ test/constants.py
@@ -7,6 +7,15 @@ try:
 except KeyError:
     version = open('version.txt').read().strip()
 
-logstash_version_string = 'logstash ' + version
+logstash_version_string = 'logstash ' + version  # eg. 'logstash 5.3.0'
+
+if '-' not in version:
+    # Then we don't have a "staging build number yet", look for one in the
+    # environment just in case.
+    try:
+        version += '-%s' % os.environ['STAGING_BUILD_NUM']  # eg. '5.3.0-d5b30bd7'
+    except KeyError:
+        pass
+
 image = 'docker.elastic.co/logstash/logstash:' + version
 container_name = 'logstash'
