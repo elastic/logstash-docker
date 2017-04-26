0905446da4be749df0add0c398335a0bbca53719
Toby McLaughlin <toby@jarpy.net>
2017-04-26 12:00:26 +1000

    Don't test for Java version

--- test/constants.py
+++ test/constants.py
@@ -1,4 +1,3 @@
 import os
 
-java_version_string = 'openjdk version "1.8.0_111"'
 logstash_version_string = 'logstash ' + os.environ['ELASTIC_VERSION']
--- test/test_basics.py
+++ test/test_basics.py
@@ -1,9 +1,5 @@
 from helpers import environment, stdout_of, stderr_of
-from constants import java_version_string, logstash_version_string
-
-
-def test_java_is_the_correct_version():
-    assert java_version_string in stderr_of('java -version')
+from constants import logstash_version_string
 
 
 def test_logstash_is_the_correct_version():
