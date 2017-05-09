9400fc96b9ca6ad535f369423d76b577ffac1365
Toby McLaughlin <toby@jarpy.net>
2017-05-09 18:45:43 +1000

    Fix tests for alpha releases

--- tests/constants.py
+++ tests/constants.py
@@ -7,14 +7,11 @@ except KeyError:
 
 logstash_version_string = 'logstash ' + version  # eg. 'logstash 5.3.0'
 
-if '-' not in version:
-    # Then we don't have a "staging build number yet", look for one in the
-    # environment just in case.
-    try:
-        if len(os.environ['STAGING_BUILD_NUM']) > 0:
-            version += '-%s' % os.environ['STAGING_BUILD_NUM']  # eg. '5.3.0-d5b30bd7'
-    except KeyError:
-        pass
+try:
+    if len(os.environ['STAGING_BUILD_NUM']) > 0:
+        version += '-%s' % os.environ['STAGING_BUILD_NUM']  # eg. '5.3.0-d5b30bd7'
+except KeyError:
+    pass
 
 image = 'docker.elastic.co/logstash/logstash:' + version
 container_name = 'logstash'
